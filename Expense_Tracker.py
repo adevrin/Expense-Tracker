4c43fa571b8f13e21e26c8509227ab47966fb94c
adevrin <adriankakahama123@gmail.com>
2026-09-17 17:36:21 +0100

    Fix expense addition and viewing logic
    Fixed logic for adding expenses to existing categories and corrected the view function to display expense amounts properly.

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -15,31 +15,32 @@ def add_expense():
     # Checks if the category was already there
     if expense_catag not in expenses:
         temp_dict[expense_catag] = {"name": expense_nme,
-                              "price": expense_price}
+                                    "price": expense_price}
         expenses.append(temp_dict)
         print(expenses)
     else:
-        expense_catag["name"] = expense_nme
-        expense_catag["price"] = expense_price
+        temp_dict = {"name": expense_nme,
+                     "price": expense_price}
+        expenses[expense_catag].append(temp_dict)
         print(f"You have added {expense_nme} with the price of {expense_price} to your expenses")
 
 
 def remove_expense(expense_rmv):
-# Asks user for the name of the expense to be removed
-        for index in range(len(expenses)): # Iterates through each category in expenses
-            for catag in expenses[index]: # Iterates through each key in the category
-                if expense_rmv in expenses[index][catag]:
-                    removed = expenses[index][catag].pop([expense_rmv])
-                    return removed
-                else:
-                    return f"You do not have any expenses called {expense_rmv}"
+    # Asks user for the name of the expense to be removed
+    for index in range(len(expenses)): # Iterates through each category in expenses
+        for catag in expenses[index]: # Iterates through each key in the category
+            if expense_rmv in expenses[index][catag]:
+                removed = expenses[index][catag].pop([expense_rmv])
+                return removed
+            else:
+                return f"You do not have any expenses called {expense_rmv}"
 
 
-def veiw_expenses():
+def view_expenses():
     for catag in expenses:
         mini_list = []
         catag = expenses.keys()
         print(f"CATERGORY ----> {catag.upper()}")
         for value in catag.values():
             mini_list.append(value)
-        print(f"{mini_list[0]} {mini_list[1]}")
+        print(f"{mini_list[0]}, £{mini_list[1]}")
